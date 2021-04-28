c510924cefd431e5b208b0c37d9aa74ade8188a7
AliZarifi <alisajjad.zarifi@gmail.com>
2021-04-28 15:59:03 +0430

    Stock catch api is created

---
+++ Frontend/src/api/StockService.js
@@ -0,0 +1,26 @@
+import axiosClient from "./axiosClient";
+
+export default {
+  addStock(stock) {
+    return axiosClient.post("/stock/add", stock);
+  },
+
+  getAllStock() {
+    return axiosClient.get("/stock/all");
+  },
+
+  deleteById(stock) {
+    return axiosClient.delete(`/saleFactor/${stock}/delete`);
+  },
+
+  getById(stockID) {
+    return axiosClient.get(`/stock/${stockID}`);
+  },
+
+  updateStock(stock) {
+    return axiosClient.put("/stock/update", stock);
+    },
+   DeleteStock(stockID) {
+    return axiosClient.get(`/stock/${stockID}/delete`);
+  }
+};
